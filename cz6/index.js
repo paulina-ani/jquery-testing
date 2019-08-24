97afb9ad305fb77ac1005b7c872485f1d34bcbd2
Paulina Aniskiewicz <aniskiewicz.paulina@gmail.com>
2019-08-24 17:20:16 +0200

    6th exercise added

---
+++ cz6/index.js
@@ -0,0 +1,20 @@
+/*
+ĆWICZENIE 6
+Stwórz komponent typu akordeon z dwoma sekcjami.
+Kliknięcie w nagłówek sekcji powinno ją rozwinąć.
+Ponownie kliknięcie w nagłówek powinno sekcję zwinąć.
+*/
+
+$("#section-1-header").click(function () {
+    $("#section-1-content").slideToggle("slow",function() {
+        alert("Hello")
+    })
+})
+
+$("#section-2-header").click(function () {
+    $("#section-2-content").slideToggle("slow",function() {
+        alert("Hi")
+    })
+})
+
+
